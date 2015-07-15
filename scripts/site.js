34593ee0d1744b067713794a5b18de15dd524a87
Ethan Gould <ethan.g.907@gmail.com>
2015-07-14 20:02:20 -0400

    included logic for slide show image cycling...thing

--- scripts/site.js
+++ scripts/site.js
@@ -27,18 +27,67 @@ module.testimonials = [{
 
 }];
 
-module.count = 0;
+module.images = [
+	'../images/angry-man.jpg', '../images/bear.jpg', '../images/joker.jpg', '../images/silver-salmon.jpg', '../images/old-man.jpg'];
+
+	module.imagez = [{
+		path: '../images/angry-man.jpg',
+		positionMobile: '0 -100px',
+		positionDektop: '0 -400px'
+	},
+	{
+		path: '../images/bear.jpg',
+		positionMobile: '0px 25px',
+		positionDektop: '0px -200px'
+	},{
+		path: '../images/joker.jpg',
+		positionMobile: '0 25px',
+		positionDektop: '0 -75px'
+	},{
+		path: '../images/silver-salmon.jpg',
+		positionMobile: '0 -100px',
+		positionDektop: '0 -250px'
+	},{
+		path: '../images/old-man.jpg',
+		positionMobile: '0 -100px',
+		positionDektop: '0 -250px'
+	}];
+
+module.testimonialCount = 0;
+module.galleryCount = 0;
 
 module.cycleTestimonials = function(testimonials) {
-	var testimonial = testimonials[module.count]
+	var testimonial = testimonials[module.testimonialCount]
 
 	_el.$testimonialContainer.hide().fadeIn(800).delay(3000).fadeOut(800);
 	_el.$testimonialContent.text('"' + testimonial.content + '"');
 	_el.$testimonialAuthor.text('- ' + testimonial.author);
 
 	module.count++;
-	if( module.count >= 3 ) {
-		module.count = 0;
+	if( module.testimonialCount >= 3 ) {
+		module.testimonialCount = 0;
+	}
+};
+
+module.cycleGallery = function(images) {
+	var image = images[module.galleryCount],
+		position;
+
+	if ( $(window).width() > 768 ) {
+		position = image.positionDektop;
+	} else {
+		position = image.positionMobile;
+	}
+
+	_el.$galleryImageContainer.hide().fadeIn(800).css({
+		'background-image': 'url("' + image.path + '")',
+		'background-position': position
+	});
+
+	module.galleryCount++;
+
+	if( module.galleryCount >= module.images.length ) {
+		module.galleryCount = 0;
 	}
 };
 
@@ -51,6 +100,7 @@ module.eventHandlers = function() {
 	module.cycleTestimonials(module.testimonials);
 	// window).scroll(module.parallax);
 	var intervalID = window.setInterval(module.cycleTestimonials, 5000, module.testimonials);
+	var intervalID = window.setInterval(module.cycleGallery, 5000, module.imagez);
 };
 
 module.init = function() {
@@ -60,6 +110,7 @@ module.init = function() {
 	_el.$testimonialContainer = $('.testimonials-container');
 	_el.$testimonialContent = $('.testimonial-content');
 	_el.$testimonialAuthor = $('.testimonial-author');
+	_el.$galleryImageContainer = $('.fullwidth-image-container');
 
 	module.eventHandlers();
 };
